5697cb952430cfde585f2ca6e55232a6766cf415
MateusKen <mateus.iochimoto@gmail.com>
2025-05-06 09:10:12 -0300

    fix: ajuste nas variáveis de ambiente

--- src/main/java/easytime/srv/api/controller/TesteController.java
+++ src/main/java/easytime/srv/api/controller/TesteController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/hello")
+@RequestMapping("/health-check")
 public class TesteController {
 
     @GetMapping()
