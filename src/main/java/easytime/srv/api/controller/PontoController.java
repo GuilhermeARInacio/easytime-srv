2e91afce6c983920b88a822f2713db6f42e4118e
vividsantos <viviane.familia834149@gmail.com>
2025-05-14 13:23:05 -0300

    fix: Atualização do Swagger com as novas implementações do sistema

--- src/main/java/easytime/srv/api/controller/PontoController.java
+++ src/main/java/easytime/srv/api/controller/PontoController.java
@@ -1,9 +1,17 @@
 package easytime.srv.api.controller;
 
 import easytime.srv.api.model.pontos.TimeLogDto;
+import easytime.srv.api.model.user.DTOUsuario;
 import easytime.srv.api.model.user.LoginDto;
 import easytime.srv.api.service.PontoService;
 import easytime.srv.api.tables.TimeLog;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -23,6 +31,40 @@ public class PontoController {
     private PontoService pontoService;
 
     @PostMapping
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Ponto batido com sucesso",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = DTOUsuario.class),
+                            examples = @ExampleObject(
+                                    value = """
+                                    {
+                                        "login":"mkenzo"
+                                    }
+                                    """
+                            )
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Usuário inválido",
+                    content = @Content(
+                            mediaType = "application/json",
+                            examples = @ExampleObject(
+                                    name = "Retorna o código 404 e a mensagem de erro",
+                                    value = """
+                                            {
+                                                "login":"abdc"
+                                            }
+                                            """
+                            )
+                    )
+            )
+    })
+    @Operation(summary = "Bater ponto", description = "Usuário envia apenas o login e o ponto é registrado com a data e hora atuais.")
+    @SecurityRequirement(name = "bearer-key")
     public ResponseEntity<?> registrarPonto(@RequestBody LoginDto login) {
         try{
             LocalDate dataHoje = LocalDate.now();
@@ -36,6 +78,18 @@ public class PontoController {
     }
 
     @DeleteMapping("/{id}")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Ponto excluido com sucesso"
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "ID não encontrado"
+            )
+    })
+    @Operation(summary = "Remover registro de batimento de ponto", description = "Usuário envia o ID do registro.")
+    @SecurityRequirement(name = "bearer-key")
     public ResponseEntity<?> removerPonto(@PathVariable Integer id) {
         pontoService.removerPonto(id);
         return ResponseEntity.ok().build();
--- src/main/java/easytime/srv/api/model/pontos/TimeLogDto.java
+++ src/main/java/easytime/srv/api/model/pontos/TimeLogDto.java
@@ -1,14 +1,20 @@
 package easytime.srv.api.model.pontos;
 
 import easytime.srv.api.tables.TimeLog;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.sql.Time;
 import java.time.LocalDate;
 
+
 public record TimeLogDto(
+        @Schema(description = "Login do usuário", example = "mkenzo")
         String login,
+        @Schema(description = "Data do ponto", example = "2023-10-01")
         LocalDate data,
+        @Schema(description = "Horário do ponto", example = "08:00:00")
         Time horarioBatida,
+        @Schema(description = "Status do ponto", example = "PENDENTE")
         TimeLog.Status status
 ) {
 
